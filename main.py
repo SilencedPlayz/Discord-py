311a85ab5d1c7ee45c2df2c7c068f7083fdf09df
Silenced CODE <sillencedplayz@gmail.com>
2025-10-26 10:55:11 +0800

    update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+handler = logging.FileHandler(filename='discord.log', encodings='utf-8', node='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
